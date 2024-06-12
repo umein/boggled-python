c5a816ae5fcc3000743caf2e4dcb3bdc11419ff9
Carl Allan G. Calsiman <2234060@slu.edu.ph>
2024-06-12 19:00:21 +0800

    establish connection

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
-print ("Hello World");
+import sys
+
+# add or modify if sys variables isn't detected
+#sys.path.append("C:/Users/czyl/Downloads/omniORB/omniORB-4.3.2/lib/python")
+#sys.path.append("C:/Users/czyl/Downloads/omniORB/omniORB-4.3.2/lib/x86_win32")
+
+from connection import Connection
+from boggled import UserNotFoundException
+
+class PlayerClient:
+    # establish connection to the server
+    def __init__(self):
+        self.connection = Connection()
+        self.player_service = self.connection.get_player_service()
+
+    # login function
+    def login(self, username, password):
+        try:
+            self.player_service.login(username, password)
+            print("Login successful.")
+        except UserNotFoundException as e:
+            print("User not found:", e)
+        except Exception as e:
+            print("An error occurred:", e)
+
+# login method invocation
+if __name__ == "__main__":
+    client = PlayerClient()
+    print("Username: ")
+    username = input()
+    print("Password: ")
+    password = input()
+    playerid = client.login(username, password)
+    print(playerid)
---
+++ connection.py
@@ -0,0 +1,42 @@
+import sys
+# add or modify if sys variables isn't detected
+#sys.path.append("C:/Users/czyl/Downloads/omniORB/omniORB-4.3.2/lib/python")
+#sys.path.append("C:/Users/czyl/Downloads/omniORB/omniORB-4.3.2/lib/x86_win32")
+
+from omniORB import CORBA
+import CosNaming
+from boggled import PlayerServices
+
+class Connection:
+    def __init__(self):
+        # initialize orb
+        args = ["-ORBInitRef", "NameService=corbaname::localhost:2000"]
+        self.orb = CORBA.ORB_init(args)
+
+        # reference to the name service
+        objref = self.orb.resolve_initial_references("NameService")
+        self.naming_context = objref._narrow(CosNaming.NamingContext)
+
+        if self.naming_context is None:
+            print("Failed to narrow the root naming context")
+            sys.exit(1)
+
+        # look up
+        name = [CosNaming.NameComponent("PlayerService", "")]
+
+        # object reference from the name service
+        try:
+            obj = self.naming_context.resolve(name)
+            self.player_service = obj._narrow(PlayerServices)
+            if self.player_service is None:
+                print("Object reference is not a PlayerServices")
+                sys.exit(1)
+        except CosNaming.NamingContext.NotFound:
+            print("Name not found")
+            sys.exit(1)
+        except Exception as e:
+            print(f"An error occurred during name resolution: {e}")
+            sys.exit(1)
+
+    def get_player_service(self):
+        return self.player_service
