5e8767819de1d0eccf782985e4f21756de539d38
Juliana April <juliana.april.cendana@gmail.com>
2024-06-13 01:13:10 +0800

    add menu

--- Main.py
+++ Main.py
@@ -17,12 +17,65 @@ class PlayerClient:
         except Exception as e:
             print("An error occurred:", e)
 
+    def menu(self):
+            while True:
+                print ("\n--- Boggled Game Menu ---")
+                print ("1. Leaderboard")
+                print ("2. Account")
+                print ("3. Start Game")
+                print ("4. Exit Game")
+                choice = input ("Enter your choice: ")
+
+                if choice == '1':
+                    self.leaderboard()
+                elif choice == '2':
+                    self.player_account()
+                elif choice == '3':
+                    self.start_game()
+                elif choice == '4':
+                    self.exit_game()
+                else:
+                    print ("Invalid input. Please try again." )
+
+    def leaderboard(self):
+        print("---- Leaderboard ----")
+        self.menu()
+        #TODO
+
+    def player_account(self):
+        print("------ Account ------")
+        self.menu()
+        #TODO
+
+    def start_game(self):
+        print("Waiting for players...")
+        self.menu()
+        #TODO
+
+    def exit_game(self):
+        while True:
+            confirm = input ("Are you sure you want to exit the game? (y/n): ")
+            if confirm == 'y':
+                print ("Exiting the game. Godbless. Ingat Ka. Mwa")
+                return
+            elif confirm == 'n':
+                print ("Returning to menu...")
+                self.menu()
+            else:
+                print ("Invalid input. Please enter 'y' for yes or 'n' for no.")
+
 # login method invocation
 if __name__ == "__main__":
     client = PlayerClient()
-    print("Username: ")
-    username = input()
-    print("Password: ")
-    password = input()
-    loggedinuser = client.login(username, password)
+    loggedinuser = None
+    while not loggedinuser:
+        print("\nUsername: ")
+        username = input()
+        print("Password: ")
+        password = input()
+        loggedinuser = client.login(username, password)
+        if not loggedinuser:
+            print ("Invalid credentials. Please please please dont prove em right.")
     print(loggedinuser)
+    if loggedinuser:
+        client.menu()
